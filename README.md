d75d0ada2f2cf3d518c4f7d12973b4fad59c6989
backtraxe <backtraxe@gmail.com>
2022-06-18 18:53:30 +0800

    upgrade to version 2.0

--- README.md
+++ README.md
@@ -1,37 +1,23 @@
-# auto_lzu_check_in_template
+# 兰州大学自动每日健康打卡
 
-兰州大学自动每日健康打卡
+## 1. `Fork`本仓库。
 
-步骤：
+<img src="imgs/step1.jpeg" />
 
-1. 新建仓库。
+<br />
 
-<img src="imgs/step1.png" width="640px" />
+## 2. 进入到`Fork`后的仓库，点击`Settings`->`Secrets`->`Actions`->`New repository secret`，添加`Name`为`USERNAME`和`PASSWORD`的两个 secret，`Value`为用户名和密码（不会泄露）。
 
-2. 选择`Import a repository`。
+<img src="imgs/step2.jpeg" />
 
-<img src="imgs/step2.png" width="640px" />
+<img src="imgs/step3.jpeg" />
 
-3. 填入`https://github.com/backtraxe/auto_lzu_check_in_template`，名字随意，选择`private`（确保密码不公开），然后导入。
+<br />
 
-<img src="imgs/step3.png" width="640px" />
+## 4. 测试。点击`Actions`->`Select workflow`->`auto_lzu_check_in`
 
-4. 进入创建的仓库。
+<img src="imgs/step4.jpeg" />
 
-<img src="imgs/step4.png" width="640px" />
+<img src="imgs/step5.jpeg" />
 
-5. 打开`main.py`。
-
-<img src="imgs/step5.png" width="640px" />
-
-6. 编辑`main.py`。
-
-<img src="imgs/step6.png" width="640px" />
-
-7. 填入自己的学号和密码（用来登录兰大个人工作台），然后保存并提交。
-
-<img src="imgs/step7.png" width="640px" />
-
-8. 编辑`.github/workflows/main.yml`，将`- cron: ''`改为`- cron: '0 17,20,23,2 * * *'`，然后保存并提交。
-
-<img src="imgs/step8.png" width="640px" />
+<img src="imgs/step6.jpeg" />
--- main.py
+++ main.py
@@ -5,13 +5,9 @@ from webdriver_manager.chrome import ChromeDriverManager
 
 import sys
 
-# 账号密码
-username = sys.argv[1]
-password = sys.argv[2]
-
 # 打开网址
 options = webdriver.ChromeOptions()
-options.add_argument('--headless') # 后台运行
+options.add_argument('--headless')  # 后台运行
 options.add_argument('--disable-gpu')
 options.add_argument("user-agent='Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36'")
 
@@ -19,8 +15,8 @@ driver = webdriver.Chrome(options=options, service=Service(ChromeDriverManager()
 driver.get('http://my.lzu.edu.cn/')
 
 # 自动登录
-driver.find_element(By.ID, 'username').send_keys(username)
-driver.find_element(By.ID, 'password').send_keys(password)
+driver.find_element(By.ID, 'username').send_keys(sys.argv[1])
+driver.find_element(By.ID, 'password').send_keys(sys.argv[2])
 driver.find_element(By.XPATH, '//button[@type="submit"]').click()
 driver.implicitly_wait(2)
 
@@ -37,24 +33,15 @@ try:
     driver.find_element(By.XPATH, '//*[text()="确定"]').click()
     driver.implicitly_wait(2)
 except:
+    # 不处理，直接跳过
     ...
 
-# 上传
+# 上报
 driver.find_element(By.XPATH, '//*[text()="上报"]').click()
 driver.implicitly_wait(2)
 
 # 判断打卡是否成功
-try:
-    driver.find_element(By.XPATH, '//*[text()="确定"]')
-    flag = 1
-except:
-    flag = 0
-
-msg = 'SUCCEEDED!' if flag == 1 else 'FAILED!'
-n = len(username) + len(msg) + 5
-print('#' * n)
-print('#', username, msg, '#')
-print('#' * n)
+driver.find_element(By.XPATH, '//*[text()="确定"]')
 
 # 退出
 driver.quit()
